aae35cc6cc93e03efadb9915987467120fa78e53
Md Ahoshan Habib <150496607+ahoshan20@users.noreply.github.com>
2025-06-24 17:54:36 +0600

    Update UserClass.php

--- app/Models/UserClass.php
+++ app/Models/UserClass.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class UserClass extends BaseModel
 {
@@ -11,11 +11,44 @@ class UserClass extends BaseModel
     protected $fillable = [
         'order_index',
         'name',
+        'icon',
         'status',
+
         'created_by',
         'updated_by',
     ];
 
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+        $this->appends = array_merge(parent::getAppends(), [
 
-    
+            'status_label',
+        ]);
+    }
+    protected $casts = [
+        'status' => 'integer',
+    ];
+
+    public const STATUS_ACTIVE = 1;
+    public const STATUS_INACTIVE = 0;
+    public static function statusList(): array
+    {
+        return [
+            self::STATUS_ACTIVE => 'Active',
+            self::STATUS_INACTIVE => 'Inactive',
+        ];
+    }
+    public function getStatusLabelAttribute()
+    {
+        return self::statusList()[$this->status];
+    }
+    public function getStatusList(): array
+    {
+        return self::statusList();
+    }
+    public function users(): HasMany
+    {
+        return $this->hasMany(User::class, 'user_class_id', 'id');
+    }
 }
