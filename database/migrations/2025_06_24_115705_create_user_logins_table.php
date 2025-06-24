97dbe5416eb5b7b38b9054115d268275b9f0043b
Md Ahoshan Habib <150496607+ahoshan20@users.noreply.github.com>
2025-06-24 18:17:47 +0600

    User Login model migration and  seeder file created

---
+++ database/migrations/2025_06_24_115705_create_user_logins_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use App\Models\UserLogin;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_logins', function (Blueprint $table) {
+            $table->id();
+            $table->integer('order_index')->default(0);
+            $table->unsignedBigInteger('user_id');
+
+            $table->string('ip');
+            $table->string('country');
+            $table->string('city');
+            $table->string('region');
+            $table->decimal('lat', 10, 7);
+            $table->decimal('lon', 10, 7);
+
+            $table->string('device');
+            $table->string('browser');
+            $table->string('platform');
+
+            $table->tinyInteger('status')->index()->default(UserLogin::STATUS_ACTIVE)->comment(UserLogin::STATUS_ACTIVE . '= True' . UserLogin::STATUS_INACTIVE . '= False');
+            $table->dateTime('last_login_at')->nullable();
+
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_logins');
+    }
+};
