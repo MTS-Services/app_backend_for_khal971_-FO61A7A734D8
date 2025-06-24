b89ec4d5d580dcf3f8857e02c373503d137d8239
Md Shariful Islam <devshariful99@gmail.com>
2025-06-24 11:32:42 +0600

    Updated

--- app/Http/Helper/main-helper.php
+++ app/Http/Helper/main-helper.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Carbon;
+
 
 function sendResponse($status, $message, $data = null, $statusCode = 200, $additional = null)
 {
@@ -12,6 +14,26 @@ function sendResponse($status, $message, $data = null, $statusCode = 200, $addit
         $responseData = array_merge($responseData, $additional);
     }
     return response()->json($responseData, $statusCode);
+}
+
+
+function timeFormat($time)
+{
+    return $time ? date('h:i A', strtotime($time)) : 'N/A';
+}
+
+function dateFormat($time)
+{
+    return $time ? date('d M, Y', strtotime($time)) : 'N/A';
+}
+
+function dateTimeFormat($time)
+{
+    return $time ? date('d M, Y h:i A', strtotime($time)) : 'N/A';
+}
+function timeFormatHuman($time)
+{
+    return Carbon::parse($time)->diffForHumans();
 }
 
 
---
+++ app/Models/BaseModel.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BaseModel extends Model
+{
+    use HasFactory;
+    protected $appends = [
+        'created_at_formatted',
+        'updated_at_formatted',
+        'deleted_at_formatted',
+
+        'created_at_human',
+        'updated_at_human',
+        'deleted_at_human',
+    ];
+
+    public function creater()
+    {
+        return $this->belongsTo(User::class, 'created_by', 'id')->select(['name', 'id']);
+    }
+
+    public function updater()
+    {
+        return $this->belongsTo(User::class, 'updated_by', 'id')->select(['name', 'id']);
+    }
+
+    // Accessor for created time
+    public function getCreatedAtFormattedAttribute()
+    {
+        return dateTimeFormat($this->created_at);
+    }
+
+    // Accessor for updated time
+    public function getUpdatedAtFormattedAttribute()
+    {
+        return $this->created_at != $this->updated_at ? dateTimeFormat($this->updated_at) : 'N/A';
+    }
+
+    // Accessor for created time human readable
+    public function getCreatedAtHumanAttribute()
+    {
+        return timeFormatHuman($this->created_at);
+    }
+
+    // Accessor for updated time human readable
+    public function getUpdatedAtHumanAttribute()
+    {
+        return $this->created_at != $this->updated_at ? timeFormatHuman($this->updated_at) : 'N/A';
+    }
+}
