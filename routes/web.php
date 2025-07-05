bada673934be9d645464665586480b8132008f6c
Md Shariful Islam <devshariful99@gmail.com>
2025-07-05 16:07:53 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Course;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
@@ -9,4 +10,7 @@ Route::get('/', function () {
 
 Route::get('get/users', function () {
     return User::all();
+})->name('get.users');
+Route::get('get/courses', function () {
+    return Course::all();
 })->name('get.users');
