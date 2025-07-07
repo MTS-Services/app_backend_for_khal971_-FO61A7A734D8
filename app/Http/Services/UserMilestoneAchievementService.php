3a39c740f0dca4f2740da45783912ffa1d7d147b
Md Ahoshan Habib <150496607+ahoshan20@users.noreply.github.com>
2025-07-07 10:25:18 +0600

    User milestone achievement on working

---
+++ app/Http/Services/UserMilestoneAchievementService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Services;
+
+class UserMilestoneAchievementService
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
