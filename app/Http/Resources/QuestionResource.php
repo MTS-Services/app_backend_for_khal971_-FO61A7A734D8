aa4e12eaa83160507f1da3ec17f7935016e43ecf
Md Ahoshan Habib <150496607+ahoshan20@users.noreply.github.com>
2025-07-10 15:09:47 +0600

    Update QuestionResource.php

--- app/Http/Resources/QuestionResource.php
+++ app/Http/Resources/QuestionResource.php
@@ -15,19 +15,93 @@ class QuestionResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return [
+        $lite = [
             'id'          => $this->id,
             'order_index' => $this->order_index,
+            'question_details_id' => $this->questionDetails->id,
             'status'      => $this->status_label,
             'statusList'  => $this->status_list,
             'language'    => translation($this->translations)->language ?? 'en',
-            'title'       => translation($this->translations)->title ?? 'N/A',
-            'answer'      => translation($this->translations)->answer ?? 'N/A',
-            'file'        => storage_url($this->questionDetails->file),
+            'title'       => translation($this->translations)->title ?? 'Not Found',
+            'answer'      => translation($this->translations)->answer ?? 'Not Found',
             'created_at'  => $this->created_at_formatted ?? dateTimeFormat(Carbon::now()),
             'updated_at'  => $this->updated_at_formatted ?? "N/A",
             'created_by'  => $this->creater?->name ?? "System",
             'updated_by'  => $this->updater?->name ?? "N/A",
         ];
+        $relations = ['question_details' => new QuestionDetailResource($this->whenLoaded('questionDetails'), 'lite')];
+        $relations['user'] = new UserResource($this->whenLoaded('user'));
+
+        return match ($this->type) {
+            'lite' => $lite,
+            default => array_merge($lite, $relations),
+        };
     }
-}
+
+
+
+
+
+//         {
+//     "success": true,
+//     "message": "Question list fetched successfully",
+//     "data": [
+//         {
+//             "id": 2,
+//             "order_index": 0,
+//             "question_details_id": 2,
+//             "status": 1,
+//             "created_at": null,
+//             "updated_at": null,
+//             "created_by": null,
+//             "updated_by": null,
+//             "status_label": "Active",
+//             "translations": [
+//                 {
+//                     "question_id": 2,
+//                     "language": "en",
+//                     "title": "Who was the first President of the United States?",
+//                     "answer": "George Washington was the first U.S. President."
+//                 }
+//             ],
+//             "question_details": {
+//                 "id": 2,
+//                 "order_index": 0,
+//                 "topic_id": 2,
+//                 "file": null,
+//                 "status": 1,
+//                 "created_at": null,
+//                 "updated_at": null,
+//                 "created_by": null,
+//                 "updated_by": null,
+//                 "translations": [
+//                     {
+//                         "question_detail_id": 2,
+//                         "language": "en",
+//                         "title": "Explain the water cycle.",
+//                         "description": "Briefly describe the stages."
+//                     }
+//                 ],
+//                 "topic": {
+//                     "id": 2,
+//                     "order_index": 0,
+//                     "course_id": 1,
+//                     "status": 1,
+//                     "created_at": null,
+//                     "updated_at": null,
+//                     "created_by": null,
+//                     "updated_by": null,
+//                     "status_label": "Active",
+//                     "translations": [
+//                         {
+//                             "topic_id": 2,
+//                             "language": "en",
+//                             "name": "Linear Equations"
+//                         }
+//                     ]
+//                 }
+//             }
+//         }
+//     ]
+// }
+    }
